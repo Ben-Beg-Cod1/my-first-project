81f83f125dd5b72ee6bcb8cd500ef644a3547edd
Benjamin <verbaanbenjamin@gmail.com>
2025-09-08 15:46:48 +0200

    added lessons

---
+++ Lessons.py
@@ -0,0 +1,19 @@
+* Saving new versions of your code
+
+* Command + S to save
+* Git add <file> to add the file to the staging area
+* Git commit -m "message" to commit the changes to the repository
+* Git push to push the changes to the remote repository
+
+* Or 
+
+* Git push -u origin <branch> to push the changes to the remote repository for the first time
+
+* Git pull to pull the changes from the remote repository
+* Git status to check the status of the repository
+* Git branch to check the branches in the repository
+* Git checkout <branch> to checkout the branch
+* Git merge <branch> to merge the branch into the current branch
+* Git log to check the history of the repository
+* Git branch -d <branch> to delete the branch
+

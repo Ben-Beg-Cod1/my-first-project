bba31113cc3c42dcb81178ef218f1305b1086e6e
Benjamin <verbaanbenjamin@gmail.com>
2025-09-09 15:19:04 +0200

    added dictionary

---
+++ dictionaries.py
@@ -0,0 +1,11 @@
+# dictionaries 
+
+Roommates_age = {"Synniva" : "26", "Alia": "23", "Leah": "12"}
+
+
+#print(Roommates_age)
+
+
+for roomie in Roommates_age: 
+    print (roomie, Roommates_age[roomie], sep= ",   ")
+

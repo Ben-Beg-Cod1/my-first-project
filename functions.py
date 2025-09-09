48918142b0d4589ff6f233c765f4af0c9ce5fa8e
Benjamin <verbaanbenjamin@gmail.com>
2025-09-09 12:18:37 +0200

    added a functions lesson

---
+++ functions.py
@@ -0,0 +1,10 @@
+def main():
+    numerator = int (input("Enter a number"))
+    denominator = int (input("Enter a second number"))
+    print("the result is", division(numerator, denominator))
+
+
+def division(a, b):
+    return a + b
+
+main()

9dcddda69298a57ca0397248655aad301e35a5a4
Benjamin <verbaanbenjamin@gmail.com>
2025-09-09 15:51:25 +0200

    saved some changes

--- dictionaries.py
+++ dictionaries.py
@@ -1,11 +1,21 @@
 # dictionaries 
 
-Roommates_age = {"Synniva" : "26", "Alia": "23", "Leah": "12"}
+#Roommates_age = {"Synniva" : "26", "Alia": "23", "Leah": "12"}
 
 
 #print(Roommates_age)
 
 
-for roomie in Roommates_age: 
-    print (roomie, Roommates_age[roomie], sep= ",   ")
+#for roomie in Roommates_age: 
+    #print (roomie, Roommates_age[roomie], sep= ",   ")
 
+
+# dictionaries with multiple key:values e.g. in a table 
+
+Roommates_info = [{"name": "Synniva", "Age": "26", "Height": "1.75m"},
+ {"name": "Alia", "Age": "24", "Height": "1.65m"}, 
+ {"name": "Leah", "Age": "23", "Height": "1.60m"}, 
+ {"name": "Heloise", "Age": "22", "Height": "1.73m"}]
+
+for i in Roommates_info: 
+    print (i["name"])
